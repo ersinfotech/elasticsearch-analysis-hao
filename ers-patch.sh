e56af7420f58580ca45853c0dff3796b538f6749
odootest <odootest2023@163.com>
2023-08-14 16:50:19 +0800

    add ers-patch.sh 5.6.8-build.sh

---
+++ ers-patch.sh
@@ -0,0 +1,12 @@
+
+echo 准备应用hao-ers分词补丁
+
+sed -i "s/String cleanedSen = clean(sentence);/String cleanedSen = clean(sentence);\n        sentence = cleanedSen;/" src/main/java/com/itenlee/search/analysis/core/Dictionary.java
+
+sed -i "s/if (vertex.length > 1 \&\& isIndexMode)/if (vertex.length >= 1 \&\& isIndexMode)/" src/main/java/com/itenlee/search/analysis/core/DijkstraSeg.java
+
+sed -i "s/if (enableSingleWord || subTerm.getEnd() - subTerm.getOffset() != 1) {/if (dictionary.getMetaWords().contains(subTerm.getText())) {\n                                termList.add(subTerm);\n                            } else if (enableSingleWord || subTerm.getEnd() - subTerm.getOffset() != 1) {/" src/main/java/com/itenlee/search/analysis/core/DijkstraSeg.java
+
+sed -i "s#<entry key=\"remoteFreqDict\"></entry>#<entry key=\"remoteFreqDict\">http://api.ersinfotech.com/helper-api/dic</entry>#" config/HaoAnalyzer.cfg.xml
+
+echo 应用hao-ers分词补丁完成
