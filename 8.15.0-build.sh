2fe65624696e7a3e4ee1e2b66e68c42e83d1b3cc
odootest2023 <odootest2023@163.com>
2024-10-07 15:58:42 +0800

    refine

--- 8.15.0-build.sh
+++
@@ -1,7 +0,0 @@
-element=8.15.0
-
-sh ers-patch.sh
-
-sed -i "s/<elasticsearch\.version>.*<\/elasticsearch\.version>/<elasticsearch\.version>$element<\/elasticsearch\.version>/g" pom.xml
-
-# mvn clean package -Dmaven.test.skip=true
